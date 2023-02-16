105c6b5bbc5afb20442f6077c9750499c2690af4
Agus SFD <124112690+agussaifuddin@users.noreply.github.com>
2023-02-16 23:11:15 +0700

    Update AgusFinance.sol

--- Agus Finance/Contracts/AgusFinance.sol
+++ Agus Finance/Contracts/AgusFinance.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./ERC20.sol";
+import "./Ownable.sol";
 
 contract AgusFinance is ERC20, Ownable {
     constructor() ERC20("Agus Finance", "AFI") {
